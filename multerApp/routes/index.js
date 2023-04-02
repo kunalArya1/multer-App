b09afbc7add5e2dec4f8e476996177c661846f07
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-02 15:03:39 +0530

    image upload module created

--- multerApp/routes/index.js
+++ multerApp/routes/index.js
@@ -1,15 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 const randombytes = require('randombytes');
+const multer = require('multer');
+
+
+//multer things
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, randombytes(8).toString('hex')+ path.extname(file.originalname));
+  }
+})
+
+const upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/random',(req,res) =>{
-  res.send(randombytes(16).toString('hex'));
+router.get('/upload',(req,res) =>{
+  res.render('upload')
+});
+
+router.post('/upload', upload.single('image'), (req,res) =>{
+  console.log(req.file);
+  res.send(req.file);
+  // res.render('imagepage',{
+  //   image : `/images/${req.file.filename}`
+  // })
 })
+
 
 
 

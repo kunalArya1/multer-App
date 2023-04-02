e91c9b70f5899b68f1cee85de78e73c613a4d5ed
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-02 13:54:51 +0530

    Random name  file things added

--- multerApp/routes/index.js
+++ multerApp/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const randombytes = require('randombytes');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/random',(req,res) =>{
+  res.send(randombytes(16).toString('hex'));
+})
+
+
 
 module.exports = router;
